76d910f83808250a4b6055b9fc1fc8a58e33ff46
Mister A <andrew.tarasov1993@gmail.com>
2022-04-16 23:20:39 +0500

    extend field length of calculation

--- forecast_app/admin.py
+++ forecast_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.contrib.gis.admin import GISModelAdmin
+from django.forms import TextInput
+from django.db import models
+
 from forecast_app.models import (
     RasterForecast,
     ForecastModel, Calculation,
@@ -10,6 +13,15 @@ from forecast_app.models import (
 
 
 # Register your models here.
-@admin.register(RasterForecast, ForecastModel, Calculation, Variable, ForecastGroup, VectorForecast)
+@admin.register(RasterForecast, ForecastModel, Variable, ForecastGroup, VectorForecast)
 class ForecastAppAdmin(GISModelAdmin):
     pass
+
+
+class CalculationModelAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': '200'})},
+    }
+
+
+admin.site.register(Calculation, CalculationModelAdmin)
