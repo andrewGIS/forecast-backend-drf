d1501c5d39d173b88811181351eab5d4196a6ce2
Mister A <andrew.tarasov1993@gmail.com>
2022-05-28 14:25:01 +0500

    feat(notification): add last name to registration

--- auth_app/serializers.py
+++ auth_app/serializers.py
@@ -20,10 +20,12 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     username = serializers.CharField(required=True, source='user.username')
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    first_name = serializers.CharField(required=True, source='user.first_name')
+    last_name = serializers.CharField(required=True, source='user.last_name')
 
     class Meta:
         model = Person
-        fields = ('username', 'password', 'telegram_login')
+        fields = ('username', 'first_name', 'last_name', 'password', 'telegram_login')
 
     # def validate(self, attrs):
     #     if attrs['password'] != attrs['password2']:
@@ -33,6 +35,8 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data['user']['username'],
+            first_name=validated_data['user']['first_name'],
+            last_name=validated_data['user']['last_name']
         )
         user.set_password(validated_data['password'])
         user.save()
